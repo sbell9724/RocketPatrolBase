319f3ded5e41c1a3aac294cfa8b96f0c62996564
sbell9724 <somibell@ucsc.edu>
2022-04-11 00:26:53 -0700

    Rocket

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ let config = {
 
 let game = new Phaser.Game(config);
 
+let keyF, keyR, keyLEFT, keyRIGHT;
+
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
