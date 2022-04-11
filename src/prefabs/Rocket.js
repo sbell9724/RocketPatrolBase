d64355990613ba1b21fb189f1ef0e22202b4c528
sbell9724 <somibell@ucsc.edu>
2022-04-11 01:14:58 -0700

    Mid-Collision Push

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -26,8 +26,12 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
 
         if(this.y = borderUISize * 3 + borderPadding) {
-            this.isFiring = false;
-            this.y = game.config.height - borderUISize - borderPadding;
+            this.reset;
         }
+    }
+
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding
     }
 }
