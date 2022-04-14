5a4d57133aaa34c9b2202f3a611c552854e64a13
sbell9724 <somibell@ucsc.edu>
2022-04-13 19:44:45 -0700

    Spaceships

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -17,6 +17,11 @@ class Play extends Phaser.Scene {
       // add rocket (p1)
       this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
 
+      // add spaceships (x3)
+      this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+      this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
+      this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+
       // green UI background
       this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
       // white borders
@@ -37,5 +42,9 @@ class Play extends Phaser.Scene {
       this.starfield.tilePositionX -= 4;
       // allows for rocket to move
       this.p1Rocket.update();
+      // update spaceships (x3)
+      this.ship01.update();
+      this.ship02.update();
+      this.ship03.update();
     }
   }
